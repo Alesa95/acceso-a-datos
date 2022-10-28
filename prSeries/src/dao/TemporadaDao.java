d6c85f6f35ecdbf9538e64fb335031525e2dfc4c
Alejandra <64591191+Alesa95@users.noreply.github.com>
2022-10-28 14:29:22 +0200

    Insertar temporadas

--- prSeries/src/dao/TemporadaDao.java
+++ prSeries/src/dao/TemporadaDao.java
@@ -1,10 +1,20 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import pojo.Temporada;
+import util.DatabaseConnection;
 
 public class TemporadaDao implements Dao<Temporada> {
+	
+	private static Connection connection;
+	
+	public TemporadaDao () {
+		
+	}
 
 	@Override
 	public ArrayList<Temporada> buscarTodos() {
@@ -21,7 +31,23 @@ public class TemporadaDao implements Dao<Temporada> {
 	@Override
 	public void insertar(Temporada t) {
 		// TODO Auto-generated method stub
+		connection = openConnection();
 		
+		String query = "insert into temporadas (num_temporada, titulo, "
+				+ "serie_id) values (?, ?, ?)";
+		
+		try {
+			PreparedStatement ps = connection.prepareStatement(query);
+			ps.setInt(1, t.getNum_temporada());
+			ps.setString(2, t.getTitulo());
+			ps.setInt(3, t.getSerie().getId()); 
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		closeConnection();
 	}
 
 	@Override
@@ -34,6 +60,22 @@ public class TemporadaDao implements Dao<Temporada> {
 	public void borrar(Temporada t) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	private static Connection openConnection() {
+		DatabaseConnection dbConnection = new DatabaseConnection();
+		connection = dbConnection.getConnection();
+		return connection;
+	}
+	
+	private static void closeConnection() {
+		try {
+			connection.close();
+			connection = null;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
